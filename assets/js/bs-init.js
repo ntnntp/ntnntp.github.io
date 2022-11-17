e804f7e61978c2708f41aacb2d76b6bde4e8e5fa
ntnntp <ntnntp@gmail.com>
2022-11-18 01:17:30 +0700

    update

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -1,7 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
+if (window.innerWidth < 768) {
+	$('[data-bss-disabled-mobile]').removeClass('animated').removeAttr('data-aos data-bss-hover-animate data-bss-parallax-bg data-bss-scroll-zoom');
+}
 
-	var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bss-tooltip]'));
-	var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-	  return new bootstrap.Tooltip(tooltipTriggerEl);
-	})
-}, false);
+$(document).ready(function(){
+	if ('AOS' in window) {
+		AOS.init();
+	}
+
+	$('[data-bss-hover-animate]')
+		.mouseenter( function(){ var elem = $(this); elem.addClass('animated ' + elem.attr('data-bss-hover-animate')) })
+		.mouseleave( function(){ var elem = $(this); elem.removeClass('animated ' + elem.attr('data-bss-hover-animate')) });
+	$('[data-bss-tooltip]').tooltip();
+});
