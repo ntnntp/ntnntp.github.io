efd7515c3d1fceaaaa6b29544829f552cf6b2213
ntnntp <ntnntp@gmail.com>
2023-02-26 04:51:59 +0700

    update uwu js functions

--- betas/assets/js/uwu.js
+++ betas/assets/js/uwu.js
@@ -5,9 +5,15 @@ var easter;
 
 function keyup(e) {
   //setting your input text to the global Javascript Variable for every key press
-  secretInput = document.getElementById("secretInput");
+  secretInput = document.getElementById('secretInput');
 
   //listens for you to press the ENTER key, at which point your web address will change to the one you have input in the search box
+  if (e.keyCode == 192) {
+    $("#fun").toast("show");
+    console.log("hello");
+  }
+
+  
   if (secretInput.value == "rickroll") {
     window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", '_blank');
     secretInput.value = "";
@@ -42,9 +48,21 @@ function keyup(e) {
     window.open("https://www.youtube.com/watch?v=YRvOePz2OqQ", '_blank');
     secretInput.value = "";
   };
-    
-  if (secretInput.value == "byebye") {
-    window.close();
+
+  if (secretInput.value == "yee") {
+    window.open("https://www.youtube.com/watch?v=q6EoRBvdVPQ", '_blank');
+    secretInput.value = "";
   };
+
+  if (secretInput.value == "aaaaaaaaaaaaaaaaaaaaaaaaaa") {
+    window.open("https://youtu.be/rvrZJ5C_Nwg?t=139", '_blank');
+    secretInput.value = "";
+  };
+
+  if (secretInput.value == "instagram") {
+    window.open("https://www.instagram.com/ntnntp", '_blank');
+    secretInput.value = "";
+  };
+    
 
 }
